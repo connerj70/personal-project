62a964ce3d99da7ce02971a350d9a7d3cd69673b
connerj70 <connerj70@outlook.com>
2017-10-11 15:44:00 -0600

    styled sell page

--- src/components/Sell/Sell.js
+++ src/components/Sell/Sell.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import backgroundPicture from '../../assets/sell-header.jpg';
 import './Sell.css';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Grid, Col, Row, Button } from 'react-bootstrap';
 import axios from 'axios';
 import { connect } from 'react-redux';
 
@@ -10,13 +10,13 @@ class Sell extends Component {
         super(props)
 
         this.state = {
-            category: 'Top',
+            category: '',
             brand: '',
             price: '',
             description:'',
             size: '',
             name: '',
-            condition: 1,
+            condition: null,
             imageURL: ''
         }
     }
@@ -27,13 +27,13 @@ class Sell extends Component {
         let user_id = this.props.user.user_id
         axios.post('http://localhost:3005/api/listings', {category: category, brand: brand, price: price, description: description, size: size, name: name, condition: condition, user_id: user_id, imageURL: imageURL });
         this.setState({
-            category: 'Top',
+            category: '',
             brand: '',
             price: '',
             description:'',
             size: '',
             name: '',
-            condition: 1,
+            condition: null,
             imageURL: ''
         })
     }
@@ -89,62 +89,81 @@ class Sell extends Component {
     render() {
         return (
             <div className='sellform-wrapper'>
-                <Jumbotron>
-                    <h1 >Turn your Closet Into Cash</h1>
-                </Jumbotron>
+                <Grid>
+                    <Jumbotron>
+                        <h1 >Turn your Closet Into Cash</h1>
+                    </Jumbotron>
 
-                <form>
-
-                <div className="details-container">
-                    <h3></h3>
-                    <div className='two-columns'>
-                        <div className='left-column'>
-                            <div>
-                                <select onChange={(e) => this.handleCategoryChange(e.target.value)} value={this.state.category}>
-                                    <option value="Top">TOP</option>
-                                    <option value="Bottom">BOTTOM</option>
-                                    <option value="Footwear">FOOTWEAR</option>
-                                    <option value="Outerwear">OUTERWEAR</option>
-                                    <option value="Accessories">ACCESSORIES</option>
-                                </select>
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>
-                        <div className='right-column'>
-                            <div className='size'>
-                                <input onChange={(e) => this.handleSizeChange(e.target.value)} value={this.state.size} placeholder="size"/>
-                            </div>
-                            <div className='name'>
-                                <input onChange={(e) => this.handleNameChange(e.target.value)} value={this.state.name} placeholder="name"/>
-                            </div>
-                        </div>
+                    <form>
+                    <Row className='show-grid'>
+                    <div className="details-container">
+                            <Col xs={12}>
+                                <h3>DETAILS</h3>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                    
+                                    <div className="sell-input">
+                                        <select className='select1' onChange={(e) => this.handleCategoryChange(e.target.value)} value={this.state.category}>
+                                            <option value="" disabled selected>Category</option>
+                                            <option value="Top">TOP</option>
+                                            <option value="Bottom">BOTTOM</option>
+                                            <option value="Footwear">FOOTWEAR</option>
+                                            <option value="Outerwear">OUTERWEAR</option>
+                                            <option value="Accessories">ACCESSORIES</option>
+                                        </select>
+                                    </div>
+                                    <div className="sell-input">
+                                        <input onChange={(e) => this.handleBrandChange(e.target.value)} value={this.state.brand} placeholder="brand"/>
+                                    </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <div className='sell-input'>
+                                    <input onChange={(e) => this.handleSizeChange(e.target.value)} value={this.state.size} placeholder="size"/>
+                                </div>
+                                <div className='sell-input'>
+                                    <input onChange={(e) => this.handleNameChange(e.target.value)} value={this.state.name} placeholder="name"/>
+                                </div>
+                            </Col>
+                           
                     </div>
-                </div>
-                    
-                
-                    <input onChange={(e) => this.handleBrandChange(e.target.value)} value={this.state.brand} placeholder="brand"/>
-                   
-                    
-                    <input onChange={(e) => this.handlePriceChange(e.target.value)} value={this.state.price} placeholder="price"/>
-                    <textarea onChange={(e) => this.handleDescriptionChange(e.target.value)} value={this.state.description} rows="4" cols="50"placeholder="description"/>
-                    <p>condition</p>
-                    <select onChange={(e) => this.handleConditionChange(e.target.value)} value={this.state.condition} name='condition'>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                    </select>
-                    <input onChange={(e) => this.handleImageURLChange(e.target.value)} value={this.state.imageURL} placeholder="image URL" />
-                    <button type='button' onClick={(e) => this.addListing(e)}>PUBLISH</button>
-                </form>
+                    </Row>
+                        
+                    <Row className='show-grid'>
+                        <Col xs={12} md={6}>
+                            <div className="sell-input">
+                                <input onChange={(e) => this.handlePriceChange(e.target.value)} value={this.state.price} placeholder="price"/>
+                            </div>
+                            <div className='sell-input'>
+                                <input onChange={(e) => this.handleImageURLChange(e.target.value)} value={this.state.imageURL} placeholder="image URL" />
+                            </div>
+                        </Col>
+                        <Col xs={12} md={6}>
+                            <div className='sell-input'>
+                            <textarea onChange={(e) => this.handleDescriptionChange(e.target.value)} value={this.state.description} rows="4" cols="50"placeholder="description"/>
+                            </div>
+                            <select className='select2'onChange={(e) => this.handleConditionChange(e.target.value)} value={this.state.condition} name='condition'>
+                                <option value="" disabled selected>Condition</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                            </select>
+
+                        </Col>
+                        </Row>
+                        <Row className='show-grid'>
+                            <Col xs={12} md={6}>
+                                <Button className='publish-btn' type='button' onClick={(e) => this.addListing(e)}>PUBLISH</Button>
+                            </Col>
+                        </Row>
+                    </form>
+                </Grid>
             </div>
         )
     }
