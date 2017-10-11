66552df6594631b4b80d729afe5c5456d492ac59
connerj70 <connerj70@outlook.com>
2017-10-11 14:36:04 -0600

    alot better on the styling and layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ import smallImage2 from '../../assets/palace.jpg';
 import carouselImage1 from '../../assets/kanye-west-bapesta.jpg';
 import carouselImage2 from '../../assets/redwings.jpg';
 import carouselImage3 from '../../assets/leatherba.jpg';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Grid, Row, Col } from 'react-bootstrap';
 import { Input } from 'semantic-ui-react';
 import SmallListing from './SmallListing/SmallListing';
 import axios from 'axios';
@@ -31,66 +31,86 @@ class Home extends Component {
 
 
     render() {
-        console.log(this.props.listingImages[0])
         return (
             <div>
-            <div className='home-container-1'>
-                <div className='large-image'>
-                    <img src={largeImage} />
-                </div>
+                <Grid>
+                    <Row className='show-grid'>
+                        <Col xs={12} lg={8}>
+                            <div className='large-image'>
+                                <img src={largeImage} />
+                            </div>
+                        </Col>
+                        <Col xs={12} lg={4}>
+                             <div className='small-image'>
+                            <img src={smallImage1} />
+                        </div>
+                        
+                        <div className='small-image'>
+                            <img src={smallImage2} />
+                        </div>
+                        </Col>
+                    </Row>
+                    <div className='home-container-1'>
                 
-                <div className='small-holder'>
-                    <div className='small-image'>
-                        <img src={smallImage1} />
+
+                    <Row className='show-grid'>
+                        <Col xs={12}>
+                            <div className='carousel-container'>
+                                <Carousel>
+                                            <Carousel.Item>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage3}/>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage2}/>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage1}/>
+                                                <Carousel.Caption>
+                                                </Carousel.Caption>
+                                            </Carousel.Item>
+                                       
+                                       
+                                            <Carousel.Item>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage1}/>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage3}/>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage2}/>
+                                               
+                                                <Carousel.Caption>
+                                                </Carousel.Caption>
+                                            </Carousel.Item>
+                                     
+                                     
+                                            <Carousel.Item>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage2}/>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage1}/>
+                                                <img  className='carousel_image' alt="900x500" src={carouselImage3}/>
+                                                <Carousel.Caption>
+                                                </Carousel.Caption>
+                                            </Carousel.Item>
+                                </Carousel>
+                            </div>
+                        </Col>
+                    </Row>
+    
+                   
                     </div>
-                    
-                    <div className='small-image'>
-                        <img src={smallImage2} />
+                        <div className='flex-container'>
+                        <Row align='center' className='show-grid search'>
+                                <Col  className='search' xs={12}>
+                                    <p>BROWSE THE FEED</p>
+                                    <Input 
+                                    icon='search'
+                                    placeholder='Search...'
+                                    />
+                                </Col>
+                        </Row>
+                        </div>
+                    <Row className='show-grid' align="center">
+                        <Col xs={12}>
+                    <div className='listing-container'> 
+                    {
+                        this.props.listings.length ? <SmallListing className='small-listing' listingImages={this.props.listingImages[0]} listings={this.props.listings}/> : <div>Loading...</div>
+                    }
                     </div>
-                </div>
-               
-                <div className='carousel-container'>
-                    <Carousel>
-                        <Carousel.Item>
-                            <img width={300} height={300} alt="900x500" src={carouselImage1}/>
-                            <img width={300} height={300} alt="900x500" src={carouselImage2}/>
-                            <img width={300} height={300} alt="900x500" src={carouselImage3}/>
-                            <Carousel.Caption>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img width={300} height={300} alt="900x500" src={carouselImage2}/>
-                            <img width={300} height={300} alt="900x500" src={carouselImage1}/>
-                            <img width={300} height={300} alt="900x500" src={carouselImage3}/>
-                            <Carousel.Caption>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img width={300} height={300} alt="900x500" src={carouselImage3}/>
-                            <img width={300} height={300} alt="900x500" src={carouselImage2}/>
-                            <img width={300} height={300} alt="900x500" src={carouselImage1}/>
-                            <Carousel.Caption>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel>
-                </div>
-  
-            </div>
-                <div className='flex-container'>
-                    <div className='input-container'>
-                        <p>BROWSE THE FEED</p>
-                        <Input 
-                        icon='search'
-                        placeholder='Search...'
-                        />
-                    </div>
-                </div>
-                <div className='listing-container'> 
-                   {
-                       this.props.listings.length ? <SmallListing className='small-listing' listingImages={this.props.listingImages[0]} listings={this.props.listings}/> : <div>Loading...</div>
-                   }
-                </div>
-                
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         )
     }
