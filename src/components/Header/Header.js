f952bf634e34dc2cf34d3a61879ac3545bd72c48
connerj70 <connerj70@outlook.com>
2017-10-25 16:13:42 -0600

    working on second menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import "./Header.css";
-import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap'
+import { Navbar, NavItem, NavDropdown, MenuItem, Nav, Grid, Row, Col } from 'react-bootstrap'
 import { Search } from 'semantic-ui-react';
 import {getUserInfo} from '../../ducks/users';
 import {connect} from 'react-redux';
@@ -8,9 +8,28 @@ import {Link} from 'react-router-dom';
 
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            activeMenu: null
+        }
+    }
 
     componentDidMount() {
         this.props.getUserInfo();
+    }
+
+    handleMouseIn(num) {
+        this.setState({
+            activeMenu: num
+        })
+    }
+
+    handleMouseOut() {
+        this.setState({
+            activeMenu: null
+        })
     }
 
     render() {
@@ -39,36 +58,68 @@ class Header extends Component {
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
-                <div className='sub-header'>
-                    <div className='sub-header-links'>
-                        <ul className='sub-header-links'>
-                            <li className='h-out'>OUTWEAR</li>
-                            <li className='h-top'>TOPS</li>
-                            <li className='h-bot'>BOTTOMS</li>
-                            <li className='h-foot'>FOOTWEAR</li>
-                            <li className='h-snea'>SNEAKERS</li>
-                            <li className='h-acce'>ACCESSORIES</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className='header-link outerwear'>
-                            
-                    </div>
-                    <div className='header-link tops'>
-
-                    </div>
-                    <div className='header-link bottoms'>
-
-                    </div>
-                    <div className='header-link footwear'>
-
-                    </div>
-                    <div className='header-link sneakers'>
-
-                    </div>
-                    <div className='header-link accessories'>
-
-                </div>
+                <Grid>
+                    <Row className='show-grid'>
+                        <Col xs={12}>
+                            <div className='sub-header'>
+                                <div className='sub-header-links'>
+                                    <ul className='sub-header-links'>
+                                        <li onMouseEnter={() => this.handleMouseIn(1)}  className='h-out'>OUTWEAR</li>
+                                        <li onMouseEnter={() => this.handleMouseIn(2)} onMouseOut={() => this.handleMouseOut()} className='h-top'>TOPS</li>
+                                        <li onMouseEnter={() => this.handleMouseIn(3)} onMouseOut={() => this.handleMouseOut()} className='h-bot'>BOTTOMS</li>
+                                        <li onMouseEnter={() => this.handleMouseIn(4)} onMouseOut={() => this.handleMouseOut()} className='h-foot'>FOOTWEAR</li>
+                                        <li onMouseEnter={() => this.handleMouseIn(5)} onMouseOut={() => this.handleMouseOut()} className='h-snea'>SNEAKERS</li>
+                                        <li onMouseEnter={() => this.handleMouseIn(6)} onMouseOut={() => this.handleMouseOut()} className='h-acce'>ACCESSORIES</li>
+                                    </ul>
+                                </div>
+                            </div>
+                                <div  onMouseOut={() => this.handleMouseOut()} className={this.state.activeMenu === 1 ? 'show' : 'hidden'}>
+                                    <Grid>
+                                        <Row className='show-grid'>
+                                            <Col xs={3}>
+                                                <h3>All Outerwear</h3>
+                                                <ul className='outerwear-list'>
+                                                    <li>Bombers</li>
+                                                    <li>Cloaks</li>
+                                                    <li>Denim Jackets</li>
+                                                    <li>Heavy Jackets</li>
+                                                    <li>Leather Jackets</li>
+                                                    <li>Light Jackets</li>
+                                                    <li>Parkas</li>
+                                                    <li>Raincoats</li>
+                                                    <li>Vests</li>
+                                                </ul>
+                                            </Col>
+                                            <Col xs={3}>
+                                            
+                                            </Col>
+                                            <Col xs={3}>
+                                            
+                                            </Col>
+                                            <Col xs={3}>
+                                            
+                                            </Col>
+                                        </Row>
+                                    </Grid>
+                                </div>
+                                <div  className={this.state.activeMenu === 2 ? 'show' : 'hidden'}>
+                                        TOPS
+                                </div>
+                                <div  className={this.state.activeMenu === 3 ? 'show' : 'hidden'}>
+                                        BOTTOMS
+                                </div>
+                                <div  className={this.state.activeMenu === 4 ? 'show' : 'hidden'}>
+                                        FOOTWEAR
+                                </div>
+                                <div  className={this.state.activeMenu === 5 ? 'show' : 'hidden'}>
+                                        SNEAKERS
+                                </div>
+                                <div  className={this.state.activeMenu === 6 ? 'show' : 'hidden'}>
+                                        ACCESSORIES
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         )
     }
